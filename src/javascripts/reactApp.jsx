189c7e3add8178d59332afb56dc18b9e9c6d70af
kasajimakengo <13570764+kasajimakengo@users.noreply.github.com>
2020-03-28 18:29:31 +0900

    remove unnessesary files

--- src/javascripts/reactApp.jsx
+++ src/javascripts/reactApp.jsx
@@ -1,17 +1,6 @@
 import ReactDom from 'react-dom';
 import * as React from 'react';
 
-import Alert from './Alert.tsx'
-
-const App = (props) => {
-  return (
-    <div style={{ color: '#000'}}>
-      Hello, React App!
-      <Alert message="Success!" />
-    </div>
-  );
-};
-
 const reactRoot = document.getElementById('react-root');
 if (reactRoot) {
   ReactDom.render(<App />, reactRoot);
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
   devtool: 'source-map',
@@ -20,15 +19,6 @@ module.exports = {
         use: [
           {
             loader: 'ts-loader',
-          },
-        ],
-      },
-      {
-        test: /\.vue/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'vue-loader',
           },
         ],
       },
@@ -102,7 +92,6 @@ module.exports = {
     ],
   },
   plugins: [
-    new VueLoaderPlugin(),
     new MiniCssExtractPlugin({
       filename: 'stylesheets/main.css'
     }),
